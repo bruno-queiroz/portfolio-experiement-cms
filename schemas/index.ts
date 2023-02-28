f725f3a5185fcd912707fddb46c82976482720f0
bruno <brunoqueiroz612@gmail.com>
2023-02-28 07:13:21 -0300

    Add all schemas to the index

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,8 @@
+import brandsLogos from './brandLogos'
+import experience from './experience'
 import skills from './skills'
+import technologySticker from './technologySticker'
+import testimonials from './testimonials'
+import work from './work'
 
-export const schemaTypes = [skills]
+export const schemaTypes = [skills, work, experience, technologySticker, testimonials, brandsLogos]
