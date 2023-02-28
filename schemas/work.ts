b1476ee409911bba910f5784e1f21c34c37d66b1
bruno <brunoqueiroz612@gmail.com>
2023-02-28 07:07:36 -0300

    Add work schema

---
+++ schemas/work.ts
@@ -0,0 +1,27 @@
+export default {
+  name: 'work',
+  type: 'document',
+  title: 'Work',
+  fields: [
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+  ],
+}
