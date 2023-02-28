d5080e6d7009735ed77396fabc7476b8f9131aeb
bruno <brunoqueiroz612@gmail.com>
2023-02-28 07:08:25 -0300

    Add technologySticker schema

---
+++ schemas/technologySticker.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'technology-sticker',
+  title: 'Technology Sticker',
+  type: 'document',
+  fields: [
+    {
+      name: 'technologyName',
+      title: 'Technology Name',
+      type: 'string',
+    },
+    {
+      name: 'technologyImage',
+      title: 'Technology Photo',
+      type: 'image',
+    },
+  ],
+}
